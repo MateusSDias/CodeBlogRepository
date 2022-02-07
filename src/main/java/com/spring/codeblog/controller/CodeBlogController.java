6e6b09a095330a1cb17a55cf9a2c85695f04bb6a
Mateus Dias <mateussdias63@gmail.com>
2022-02-07 11:30:55 -0300

    usando thymeleaf

---
+++ src/main/java/com/spring/codeblog/controller/CodeBlogController.java
@@ -0,0 +1,33 @@
+package com.spring.codeblog.controller;
+
+
+import com.spring.codeblog.model.Post;
+import com.spring.codeblog.repository.CodeblogRepository;
+import com.spring.codeblog.service.CodeblogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+public class CodeBlogController {
+
+    @Autowired
+    CodeblogService codeblogService;
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @RequestMapping(value = "posts", method = RequestMethod.GET)
+    public ModelAndView getPost(){
+        ModelAndView mv = new ModelAndView("posts");
+        List<Post> posts = codeblogService.findAll();
+        mv.addObject("posts", posts);
+        return mv;
+    }
+}
